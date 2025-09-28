ec5b10832abf16b09b16d6cf582d29cc86822f53
Sushkdm <kadamsushant760@gmail.com>
2025-09-28 20:13:01 +0530

    i have add the student class and the NewGetPackges for Entity FreamWork

---
+++ myproject/CrudMVCEF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudMVCEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
