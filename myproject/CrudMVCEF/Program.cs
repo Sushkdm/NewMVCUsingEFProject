5986b2bbaa0d07c44056066a6092a57716171de4
Sushant kadam <kadamsushant760@gmail.com>
2025-09-29 15:39:06 +0530

    Revert "Adding the files ef"

--- myproject/CrudMVCEF/Program.cs
+++ myproject/CrudMVCEF/Program.cs
@@ -1,12 +1,7 @@
-using CrudMVCEF.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CrudOperation")));
 
 var app = builder.Build();
 
